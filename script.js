98237cac5177aec7fea2259e9ef5f15b25869ae9
tmcode7 <mawuenatord7@gmail.com>
2022-09-02 22:23:04 +0000

    solved array sum and array reverse

--- script.js
+++ script.js
@@ -83,3 +83,28 @@ function countChar(string, char){
 console.log(countBs("BBBC"));
 
 console.log(countChar("kakkerlak", "k"));
+
+//7. Sum of a range
+
+//a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+function range(start, end, step = start < end ? 1 : -1) {
+    let array = [];
+  
+    if (step > 0) {
+      for (let i = start; i <= end; i += step) array.push(i);
+    } else {
+      for (let i = start; i >= end; i += step) array.push(i);
+    }
+    return array;
+  }
+
+// a sum function that takes an array of numbers and returns the sum of these numbers.
+function sum(array) {
+    let total = 0;
+    for (let value of array) {
+      total += value;
+    }
+    return total;
+  }  
+
+  
